af4685413f31aeb47043ef6126c5d8c1c9a1bca8
chenJz1012 <597160667@qq.com>
2017-03-31 21:32:54 +0800

    fix

--- src/main/java/com/orange/system/api/common/NoneAuthController.java
+++ src/main/java/com/orange/system/api/common/NoneAuthController.java
@@ -1,11 +1,18 @@
 package com.orange.system.api.common;
 
+import com.orange.common.tools.cache.RedisCache;
+import com.orange.common.tools.captcha.service.Captcha;
+import com.orange.common.tools.captcha.utils.ImageUtils;
 import com.orange.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by chenguojun on 8/31/16.
@@ -16,10 +23,19 @@ public class NoneAuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisCache redisCache;
+
     @RequestMapping("/unique")
     @ResponseBody
     public Object unique(@RequestParam(value = "loginName", required = false) String loginName) {
         int count = userService.countByLoginName(loginName);
         return count > 0 ? false : true;
+    }
+    @RequestMapping("/captcha")
+    public void captcha(@RequestParam("vkey") String vkey, HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse) throws IOException {
+        Captcha captcha = ImageUtils.getCaptcha(httpServletResponse);
+        redisCache.set(vkey, captcha.getChallenge(), 120);
     }
 }
